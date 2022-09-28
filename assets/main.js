c0c9dbaeffb8f707ab45acdc20f070ef70369712
Albertah-R <albertah.razaf@gmail.com>
2022-09-28 21:32:35 +0200

    WIP : update html, css and js

---
+++ assets/main.js
@@ -0,0 +1,19 @@
+/*Définir les variables et  récupérer les informations dans le formulaire*/
+
+
+/*Voir les détails des cartes front et back se mettre à jour en temps réel*/
+function updateCard() { 
+  /* Vérifier si le nom d'utilisateur est vide et si les informations respectent le format. Input vide + mauvais format, il affichera un message d'alerte. */
+  if (u) {
+    ;
+    alert("Please enter your name");
+  } else /*Afficher le nom de l'utilisateur sur la card-front*/ {
+    
+  }
+}
+
+/*Message d'erreur lorsque le champs est vide*/
+/*Message d'erreur lorsque la valeur n'est pas au bon format*/
+/*Afficher la mise en page optimale*/
+
+
